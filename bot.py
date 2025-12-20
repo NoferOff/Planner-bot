8000a477e9ff36802d7e9258ce4d711c5fbf31b3
NoferOff <denis.horpynych99@gmail.com>
2025-12-20 22:53:15 +0200

    Add new library and configuration file

---
+++ bot.py
@@ -0,0 +1,6 @@
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import ApplicationBuilder, CommandHandler, CallbackQueryHandler, ContextTypes
+from dotenv import load_dotenv
+import os
+
+load_dotenv() 
